dd83bc016862499c3cfdf15ed906f579d2abea83
etaws <etawsz@gmail.com>
2023-02-26 17:23:18 +0800

    more comments

--- src/disappear.rs
+++ src/disappear.rs
@@ -3,12 +3,14 @@ pub fn disappear_number(a: &mut [i32]) -> Vec<usize> {
 
     let mut i = 0;
     while i < a.len() {
+        // 第几个数已经存在？如果是第 1 个数存在，那么 j 就是 0
         let j: usize = if a[i] < 0 {
             (0 - a[i] - 1).try_into().unwrap()
         } else {
             (a[i] - 1).try_into().unwrap()
         };
 
+        // 如果 j 这个数已经存在，那么把 a[j] 改为负数，作为 j 存在的标志
         if a[j] > 0 {
             assert!(a[j] > 0 && a[j] <= a.len().try_into().unwrap());
             a[j] = 0 - a[j];
@@ -17,6 +19,7 @@ pub fn disappear_number(a: &mut [i32]) -> Vec<usize> {
         i += 1;
     }
 
+    // 如果 a[j] 还是正数，说明第 j + 1 个数不存在，放如结果中
     for (j, n) in a.iter().enumerate() {
         if *n > 0 {
             v.push(j + 1);
