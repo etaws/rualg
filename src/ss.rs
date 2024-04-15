fe514c79c8fff42705845f4503eb251af019d0c1
etaws <etawsz@gmail.com>
2024-04-15 14:47:32 +0800

    pass daily_temperatures
    perf it

--- src/ss.rs
+++ src/ss.rs
@@ -52,6 +52,33 @@ pub fn is_valid(s: String) -> bool {
     false
 }
 
+pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+    let mut r = vec![0; temperatures.len()];
+
+    let mut stack: Vec<(i32, usize)> = Vec::with_capacity(temperatures.len());
+
+    for (i, n) in temperatures.iter().enumerate() {
+        let v = *n;
+        if stack.is_empty() {
+            stack.push((v, i));
+            continue;
+        }
+
+        while !stack.is_empty() {
+            let top = stack.last().unwrap();
+            if v > top.0 {
+                r[top.1] = (i - top.1) as i32;
+                stack.pop();
+            } else {
+                break;
+            }
+        }
+        stack.push((v, i));
+    }
+
+    r
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -66,5 +93,17 @@ mod tests {
         // negative
         assert!(!is_valid("(]".to_string()));
         assert!(!is_valid("{[{[{}]".to_string()));
+    }
+
+    #[test]
+    fn check_daily_temperatures() {
+        assert_eq!(
+            daily_temperatures(vec![73, 74, 75, 71, 69, 72, 76, 73]),
+            vec![1, 1, 4, 2, 1, 1, 0, 0]
+        );
+
+        assert_eq!(daily_temperatures(vec![30, 40, 50, 60]), vec![1, 1, 1, 0]);
+
+        assert_eq!(daily_temperatures(vec![30, 60, 90]), vec![1, 1, 0]);
     }
 }
