620a4a77c211c12747b078f52e95a342467c3285
etaws <etawsz@gmail.com>
2024-05-22 13:06:49 +0800

    pass length_of_lis

--- src/stairs.rs
+++ src/stairs.rs
@@ -615,11 +615,37 @@ pub fn can_partition(nums: Vec<i32>) -> bool {
     false
 }
 
+pub fn length_of_lis(nums: Vec<i32>) -> i32 {
+    let mut dp: Vec<i32> = vec![1; nums.len() + 1];
+
+    let mut max = 1;
+    for i in 1..nums.len() {
+        for j in 0..i {
+            if nums[i] > nums[j] {
+                dp[i] = dp[i].max(dp[j] + 1);
+            }
+        }
+
+        if max < dp[i] {
+            max = dp[i];
+        }
+    }
+    max
+}
+
 #[cfg(test)]
 mod tests {
 
     use super::*;
     use std::collections::HashMap;
+
+    #[test]
+    fn check_length_of_lis() {
+        assert_eq!(length_of_lis(vec![1, 3, 6, 7, 9, 4, 10, 5, 6]), 6);
+        assert_eq!(length_of_lis(vec![7, 7, 7, 7, 7, 7, 7]), 1);
+        assert_eq!(length_of_lis(vec![0, 1, 0, 3, 2, 3]), 4);
+        assert_eq!(length_of_lis(vec![10, 9, 2, 5, 3, 7, 101, 18]), 4);
+    }
 
     #[test]
     fn check_can_partition() {
