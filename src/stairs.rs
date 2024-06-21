de8641d1578ce64fb9510952d4ea72416ef575d6
etaws <etawsz@gmail.com>
2024-06-21 15:09:38 +0800

    pass min_distance

--- src/stairs.rs
+++ src/stairs.rs
@@ -829,11 +829,49 @@ pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
     f[n][m]
 }
 
+pub fn min_distance(word1: String, word2: String) -> i32 {
+    let m = word1.len();
+    let n = word2.len();
+    let mut dp = vec![vec![0; n + 1]; m + 1];
+
+    (0..=m).for_each(|i| {
+        dp[i][0] = i;
+    });
+
+    (0..=n).for_each(|j| {
+        dp[0][j] = j;
+    });
+
+    for i in 1..=m {
+        for j in 1..=n {
+            if word1.as_bytes()[i - 1] == word2.as_bytes()[j - 1] {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = std::cmp::min(
+                    std::cmp::min(dp[i - 1][j] + 1, dp[i][j - 1] + 1),
+                    dp[i - 1][j - 1] + 1,
+                );
+            }
+        }
+    }
+
+    dp[m][n] as i32
+}
+
 #[cfg(test)]
 mod tests {
 
     use super::*;
     use std::collections::HashMap;
+
+    #[test]
+    fn check_min_distance() {
+        assert_eq!(
+            min_distance("intention".to_string(), "execution".to_string()),
+            5
+        );
+        assert_eq!(min_distance("horse".to_string(), "ros".to_string()), 3);
+    }
 
     #[test]
     fn check_longest_common_subsequence() {
